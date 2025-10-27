e221d9783a0aea4a37515df041b743105c2de185
Zelelo622 <egorcom27@gmail.com>
2025-10-28 01:49:15 +0300

    feat: added user service

---
+++ nestjs-server/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "./../../prisma/__generated__/index.d";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  public async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  public async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
